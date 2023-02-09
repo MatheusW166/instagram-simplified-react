f2b09a0c58b2cf6962ba33ea9dc16e316b82717b
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-02-09 15:49:27 -0300

    Publications added in db

--- src/db/db.js
+++ src/db/db.js
@@ -12,8 +12,92 @@ const STORIES = [
   { text: "memeriago.", image: "./images/memeriagourmet_1.png" },
 ];
 
+const PUBS = [
+  {
+    user: { name: "palpatine.oficial", image: "./images/imperador.png" },
+    image: "./images/palpatine.jpg",
+    likes: {
+      firstUserLike: { name: "anakin.skywalker", image: "./images/anakin.jpg" },
+      totalLikes: 1950001,
+    },
+    caption: {
+      text: "Não existe UM Lorde Sith vivo mais honesto do que eu!",
+      tags: ["#votepalpatine", "#pelademocracia", "#liberdade"],
+    },
+    pubComments: {
+      totalComments: 666750,
+      comments: [],
+    },
+    createdAt: "HÁ 5 MINUTOS",
+  },
+  {
+    user: { name: "kleiton.of.war", image: "./images/kratos.jpg" },
+    video: "./images/video.mp4",
+    likes: {
+      firstUserLike: { name: "odin_father69", image: "./images/odin.webp" },
+      totalLikes: 7569,
+    },
+    caption: {
+      text: "Atreus 100% pistola brincando com as gaivotas no riacho da @freya heauehauehaueh",
+      tags: [],
+    },
+    pubComments: {
+      totalComments: 14,
+      comments: [
+        {
+          user: { name: "freya", image: "freya" },
+          text: "Mais uma vez vocês dois matando meus animais 🙄",
+        },
+        {
+          user: { name: "thor_odin.son", image: "thor_odin.son" },
+          text: "⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡",
+        },
+      ],
+    },
+    createdAt: "HÁ 2 HORAS",
+  },
+  {
+    user: { name: "meowed", image: "./images/meowed_1.png" },
+    image: "./images/gato-telefone_1.png",
+    likes: {
+      firstUserLike: { name: "uzumaki.naruto", image: "./images/naruto.jpg" },
+      totalLikes: 47.001,
+    },
+    caption: {
+      text: "Os gatos vão dominar o mundo?",
+      tags: [],
+    },
+    pubComments: {
+      totalComments: 0,
+      comments: [],
+    },
+    createdAt: "HÁ 37 SEGUNDOS",
+  },
+  {
+    user: { name: "adorable_animals", image: "./images/adorableanimals_1.png" },
+    image: "./images/dog_1.png",
+    likes: {
+      firstUserLike: {
+        name: "memeriagourmet",
+        image: "./images/memeriagourmet_1.png",
+      },
+      totalLikes: 18,
+    },
+    caption: {
+      text: "Soninho da beleza 🥰🥰",
+      tags: [],
+    },
+    pubComments: {
+      totalComments: 0,
+      comments: [],
+    },
+    createdAt: "HÁ 1 DIA",
+  },
+];
+
 const db = {
   getStories: () => STORIES.map((s) => ({ ...s })),
+  getPubs: () => PUBS,
 };
 
 export default db;
