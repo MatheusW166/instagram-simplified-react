c4e1edaa29011cc879be6ebf559f36665ae86df1
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-02-10 13:21:39 -0300

    Change name and photo

--- src/components/AsideUser.jsx
+++ src/components/AsideUser.jsx
@@ -1,13 +1,35 @@
-import UserName from "./UserName";
-import UserImage from "./UserImage";
+import { useState } from "react";
+import Icon from "./Icon";
 
 export default function AsideUser(props) {
-  const { user } = props;
+  const [user, setUser] = useState(props.user);
+
+  const changeName = () => {
+    const newName = prompt("Digite seu novo nome.");
+    if (!newName.trim()) return;
+    setUser({ ...user, name: newName });
+  };
+  const changePhoto = () => {
+    const newPhoto = prompt("Coloque a url da sua nova foto.");
+    if (!newPhoto.trim()) return;
+    setUser({ ...user, image: newPhoto });
+  };
+
   return (
     <section className="my-user">
-      <UserImage user={user} />
+      <img
+        onClick={changePhoto}
+        alt="img"
+        className="avatar"
+        src={user.image}
+      />
       <div>
-        <UserName user={user} />
+        <div className="edit-name">
+          <p>{user.name}</p>
+          <div onClick={changeName}>
+            <Icon name="pencil" />
+          </div>
+        </div>
         <p>{user.displayName}</p>
       </div>
     </section>
